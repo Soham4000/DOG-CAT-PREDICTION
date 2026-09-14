01dbf9cc8db9ab28348821c7365293c2113542ce
Soham4000 <soham.sinha1202@gmail.com>
2026-09-14 11:40:48 +0530

    Update DC.py

--- DC.py
+++ DC.py
@@ -65,7 +65,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
-    st.image(img, caption="Uploaded image", use_column_width=True)
+    st.image(img, caption="Uploaded image", use_container_width=True)
 
     img_resized = img.resize((IMG_SIZE, IMG_SIZE))
     img_array = np.array(img_resized).astype(np.float32) / 255.0
